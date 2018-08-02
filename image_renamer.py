1c2bf7c81857fb073cb9928f082d84665944d4f7
blair gemmer <blairg23@gmail.com>
2018-08-02 16:21:37 -0600

    Added if/else to catch if datetime doesn't exist in the image and then print out the get_exif() info with the filename if it doesn't

--- image_renamer.py
+++ image_renamer.py
@@ -24,18 +24,24 @@ if __name__ == '__main__':
     for filepath in filepaths:
         filename, extension = os.path.splitext(filepath)
         # print(get_exif(filepath)['DateTimeOriginal'])
-        date_taken = datetime.datetime.strptime(get_exif(filepath)['DateTimeOriginal'], '%Y:%m:%d %H:%M:%S')
+        if 'DateTimeOriginal' in get_exif(filepath):
+            image_datetime = get_exif(filepath)['DateTimeOriginal']
+            date_taken = datetime.datetime.strptime(image_datetime, '%Y:%m:%d %H:%M:%S')
 
-        new_filename = date_taken.strftime('%Y-%m-%dT%H.%M.%S')
+            new_filename = date_taken.strftime('%Y-%m-%dT%H.%M.%S')
+            
+            new_filepath = os.path.join(input_directory, new_filename+extension)
 
-        new_filepath = os.path.join(input_directory, new_filename+extension)
+            number = 0
 
-        number = 0
+            while os.path.exists(new_filepath):
+                number += 1
+                # new_filename, extension = os.path.splitext(new_filepath)
+                new_new_filename = new_filename + '.' + str(number)
+                new_filepath = os.path.join(input_directory, new_new_filename + extension)
 
-        while os.path.exists(new_filepath):
-            number += 1
-            # new_filename, extension = os.path.splitext(new_filepath)
-            new_new_filename = new_filename + '.' + str(number)
-            new_filepath = os.path.join(input_directory, new_new_filename + extension)
-
-        os.rename(filepath, new_filepath)
+            os.rename(filepath, new_filepath)
+        else:
+            print('filename:', filename)
+            print('get_exif(filepath)', get_exif(filepath))
+            print('\n')
